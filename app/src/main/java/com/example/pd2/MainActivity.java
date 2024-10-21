36f0ff2d7cd9cc617cd3645c6311ff4ee2b2e18b
ignelitv <igne.litvaityte@knf.stud.vu.lt>
2024-10-21 22:22:58 +0300

    Code written during the lecture

--- app/src/main/java/com/example/pd2/MainActivity.java
+++ app/src/main/java/com/example/pd2/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.pd2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.pd2.utils.TextCounter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edUserInput;
+    private TextView tvResult;
+    private Spinner spCountOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.edUserInput = findViewById(R.id.edUserInput);
+        this.tvResult = findViewById(R.id.tvResult);
+
+        this.spCountOption = findViewById(R.id.spCountOption);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.counting_options,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spCountOption.setAdapter(adapter);
+
+    }
+
+    public void onBtnCountClick(View view) {
+        if(this.spCountOption.getSelectedItem().toString().equals("Chars")){
+            String userInputPhrase = this.edUserInput.getText().toString();
+            int result = TextCounter.getCharsCount(userInputPhrase);
+            this.tvResult.setText(String.valueOf(result));
+        }
+        else{
+            this.tvResult.setText(R.string.message_not_implemented);
+        }
     }
 }
---
+++ app/src/main/java/com/example/pd2/utils/TextCounter.java
@@ -0,0 +1,8 @@
+package com.example.pd2.utils;
+
+public class TextCounter {
+
+    public static int getCharsCount(String inputPhrase){
+        return inputPhrase.length();
+    }
+}
